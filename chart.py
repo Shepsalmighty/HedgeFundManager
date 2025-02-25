1c771dec2b654c038eabdd6d4d6dcb6125832a67
sheps <sheps@twitch.tv>
2025-02-25 18:38:36 +0000

    remove redundant/commented code

--- chart.py
+++ chart.py
@@ -1,21 +1,17 @@
-import sys
-
-from PySide6.QtCharts import QCandlestickSeries, QCandlestickSet, QChart, QChartView, QValueAxis, QDateTimeAxis
-from PySide6.QtCore import Qt, QDateTime, QTimer, QRect, QPoint, QPointF, QRectF
-from PySide6.QtGui import QColor, QPainter
-from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget
 from typing import Callable
-from functools import partial
 
-
+from PySide6.QtCharts import QCandlestickSet, QChart, QChartView, QValueAxis, QDateTimeAxis
+from PySide6.QtCore import Qt, QDateTime, QTimer
+from PySide6.QtGui import QColor, QPainter
 
 
 def run_animation(steps: list[tuple[Callable[[], None], int]]):
-    """Tranq - Callable[[], None] takes no arguments and returns nothing"""
+    """Callable[[], None] takes no arguments and returns nothing
+        takes our animation step (new candlesticks) and a time delay"""
     for func, delay in steps:
-
         timer = QTimer()
         timer.singleShot(delay, func)
+
 
 class Chart():
     def __init__(self, stock, info_label, window):
@@ -30,31 +26,28 @@ class Chart():
         self.yaxis_upper_range = 0
         self.y_axis = QValueAxis()
         # create starting candle stick data of 30 candles - line 43
-        self.create_candles(self)
+        self.create_candles()
 
-
-
-    def create_candles(self, chart_window):
-
+    def create_candles(self):
         self.stock.stock_series.setName(self.stock.name)
         # setting colour for bull/bear candles
         self.stock.stock_series.setIncreasingColor(QColor(0, 255, 113, 255))
         self.stock.stock_series.setDecreasingColor(QColor(218, 13, 79, 255))
 
-        # creates the initial chart data - currently set to 30 candles and increments self.index by same
+        # creates the initial chart data - currently set to 30 candles and increments self.stock.index by same
         for i in range(30):
-            self.add_new_candle(chart_window)
+            self.add_new_candle()
 
     # Helper function to create a hover handler without using a lambda
     def create_hover_handler(self, cs, idx):
         def handler(hovered):
-            # chart_window.update_label(hovered, cs, idx)
             self.update_label(hovered, cs, idx)
 
         return handler
 
-    def add_new_candle(self, chart_window):
+    def add_new_candle(self):
 
+        # used to create the offset for our Y-axis upper and lower range candles see 76-78
         def lower_and_upper_range(low, high):
             middle = (low + high) / 2
             half_span = (high - low) / 2
@@ -80,18 +73,15 @@ class Chart():
             self.candlestick_high = max(self.stock.highs[self.stock.index - self.candles_on_screen:self.stock.index])
             self.candlestick_low = min(self.stock.lows[self.stock.index - self.candles_on_screen:self.stock.index])
 
-            self.yaxis_lower_range, self.yaxis_upper_range = lower_and_upper_range(self.candlestick_low, self.candlestick_high)
+            self.yaxis_lower_range, self.yaxis_upper_range = lower_and_upper_range(self.candlestick_low,
+                                                                                   self.candlestick_high)
+            #updates y_axis to the candle high/low of the currently shown candles
             self.y_axis.setRange(self.yaxis_lower_range, self.yaxis_upper_range)
-
 
     def create_view(self):
         """displays the candlestick charts, and sets the legend, axes etc.
         returns chart_view"""
         self.chart.addSeries(self.stock.stock_series)
-
-        # TODO newly generated candlesticks do not have hover-over data
-        # creating a arg-less function using partial as run_animation() cannot take funcs with args
-        new_candle = partial(self.add_new_candle, self.window)
 
         self.chart.setTitle(f"{self.stock.name} data from {self.stock.from_date}")
         self.chart.setAnimationOptions(QChart.AnimationOption.SeriesAnimations)
@@ -105,12 +95,13 @@ class Chart():
             self.stock.dates[0] // 1000)  # this function expects milliseconds
         max_date = QDateTime.fromSecsSinceEpoch(self.stock.dates[-1] // 1000)
 
-        # adds 1 day to start and end so candlesticks don't go up to the borders (visually more pleasing)
-        min_date = min_date.addSecs(-3600 * 24)
+        # adds leading/trailing space in "days" so candlesticks don't go up to the borders (visually more pleasing)
+        min_date = min_date.addSecs(-7200 * 24)
         max_date = max_date.addSecs(3600 * 24)
 
         x_axis.setRange(min_date, max_date)
 
+        #setting the value spacing of the x-axis to 7 - TickCount(7)
         x_axis.setTickCount(7)
         self.chart.addAxis(x_axis, Qt.AlignmentFlag.AlignBottom)
         self.stock.stock_series.attachAxis(x_axis)
@@ -118,8 +109,6 @@ class Chart():
         self.y_axis = QValueAxis()
         self.y_axis.setTitleText("Stock Price")
         self.y_axis.setLabelFormat("%.2f")
-        # self.chart.setPlotArea(self, QRect(QPointF, self.yaxis_upper_range, QPointF, max_date))
-        # self.y_axis.setRange(self.yaxis_lower_range, self.yaxis_upper_range)
         self.chart.addAxis(self.y_axis, Qt.AlignmentFlag.AlignLeft)
         self.stock.stock_series.attachAxis(self.y_axis)
 
@@ -131,18 +120,17 @@ class Chart():
         animation_list = []
         for i in range(self.stock.index):
             j = (i + 1) * 1000
-            animation_list.append((new_candle, j))
+            animation_list.append((self.add_new_candle, j))
 
         run_animation(animation_list)
 
-        # Create and return chart view
+        # Create and return chart view with antiailiasing
         chart_view = QChartView(self.chart)
-        # adding antialiasing
         chart_view.setRenderHint(QPainter.Antialiasing)
 
         return chart_view
 
-
+# updates the OHLC data in a new widget if the candle is moused over
     def update_label(self, hovered, candlestick_set, index):
         if hovered:
             date = QDateTime.fromMSecsSinceEpoch(self.stock.dates[index]).toString("yyyy-MM-dd")
