3f1084c082ac3db181cfecfdbdc5499f2eea97d7
sheps <sheps@twitch.tv>
2025-03-02 14:43:39 +0000

    add initialised func

--- portfolio.py
+++ portfolio.py
@@ -1,4 +1,6 @@
-class Portfolio():
+import json
+
+class Portfolio:
     def __init__(self):
         #TODO add 4% APY interest on cash
         self.cash = 10000000
@@ -6,7 +8,15 @@ class Portfolio():
 
     def __str__(self):
         return (f"you have $$${round(self.cash, 2)} burning a hole in your wallet, \n"
+                # round(self.holdings.get('cash'),2)
                 f"and {self.holdings} stonks")
 
+
+    def initialised(self):
+        """returns False if no holdings - zero trades have been made"""
+        return len(self.holdings) is not 0
+
+mytest = Portfolio()
+print(mytest.initialised())
 
 
