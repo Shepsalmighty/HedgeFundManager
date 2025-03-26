5ecf9f92fa330b438cbb8c6519b210a818e24cc9
sheps <sheps@twitch.tv>
2025-03-26 18:52:34 +0000

    increment x_axis

--- chart.py
+++ chart.py
@@ -11,8 +11,9 @@ class Animation:
     def __init__(self, steps: list[tuple[Callable[[], None], float]]):
         """Callable[[], None] takes no arguments and returns nothing
                 takes our animation step (new candlesticks) and a time delay"""
+        super().__init__()
         self.steps: list[tuple[Callable[[], None], float]] = steps
-        self.thread: threading.Thread | None = None
+        self.thread1: threading.Thread | None = None
         self.__pause_lock: threading.Lock = threading.Lock()
 
     def __run(self):
@@ -21,35 +22,33 @@ class Animation:
         # they are executed at the right time
         for func, delay in self.steps:
             time.sleep(delay)
-            #creating a lock/unlock cycle interrupted by pause() and resume() calls in buy window - pausing animation
+            # creating a lock/unlock cycle interrupted by pause() and resume() calls in buy window - pausing animation
             with self.__pause_lock:
                 func()
-
-
 
     def join(self):
         """ waits for the animation to complete or instantly returns if it is not started/running """
         # joins threads once animation is completed
-        if self.thread:
-            self.thread.join()
+        if self.thread1:
+            self.thread1.join()
 
     def start(self):
         """ starts a new thread that executes the specified steps at the right time """
         # create a new thread that executes all the specified animation steps and store thread-handle
-        self.thread = threading.Thread(target=self.__run)
-       # start the animation thread
-        self.thread.start()
+        self.thread1 = threading.Thread(target=self.__run)
+        # start the animation thread
+        self.thread1.start()
 
     def pause(self):
         """ pauses the timer thread that was started using the start() method
         (if no timer is running, this method does nothing) """
-        if self.thread:
+        if self.thread1:
             self.__pause_lock.acquire()
 
     def resume(self):
         """resumes a paused timer thread as if it was exactly at the point in time that it was paused at
         (does nothing if no paused timer thread exists)"""
-        if self.thread:
+        if self.thread1:
             self.__pause_lock.release()
 
 
@@ -60,7 +59,7 @@ class Chart:
         :param stock: stock data from yfinance - from ``stock.py``
         :param window: inherited from ``__main__`` see QMainWindow
         """
-        #chart instance for candle sticks
+        # chart instance for candle sticks
         self.chart: QChart = QChart()
         self.stock = stock
         self.cash = cash
@@ -69,13 +68,24 @@ class Chart:
         self.candles_on_screen: int = 0
         self.candlestick_high = self.stock.highs[0]
         self.candlestick_low = self.stock.lows[0]
+        #create x_axis and set legend
+        self.x_axis: QDateTimeAxis = QDateTimeAxis()
+        self.x_axis.setFormat("MMM-yy")
+        self.x_axis.setTitleText("Date")
+        # mark 7 dates on x_axis - TickCount(7)
+        self.x_axis.setTickCount(7)
+        #create y_axis and set starting ranges
+        self.y_axis: QValueAxis = QValueAxis()
         self.yaxis_lower_range = 0
         self.yaxis_upper_range = 0
-        self.y_axis: QValueAxis = QValueAxis()
         # event thread to stop/start candle stick animations while an order is being placed
         self.animation: Animation | None = None
+        #vars for later
+        self.day_in_seconds = 86400
+        self.off_set = 3
         # create starting candle stick data of 30 candles - line 43
         self.create_candles()
+
 
     def create_candles(self):
         self.stock.stock_series.setName(self.stock.name)
@@ -91,6 +101,7 @@ class Chart:
     def create_hover_handler(self, cs, idx):
         def handler(hovered):
             self.update_label(hovered, cs, idx)
+
         return handler
 
     def add_new_candle(self):
@@ -117,7 +128,7 @@ class Chart:
             if self.candles_on_screen < 25:
                 self.candles_on_screen += 1
 
-#TODO: look at .remove() - not removing left most candles causing data to compress
+            # TODO: look at .remove() - not removing left most candles causing data to compress
             if self.stock.index >= self.candles_on_screen:
                 oldcandlestick = QCandlestickSet(self.stock.dates[self.stock.index - self.candles_on_screen])
                 oldcandlestick.setOpen(self.stock.opens[self.stock.index - self.candles_on_screen])
@@ -134,9 +145,19 @@ class Chart:
 
             self.yaxis_lower_range, self.yaxis_upper_range = lower_and_upper_range(self.candlestick_low,
                                                                                    self.candlestick_high)
-            #updates y_axis to the candle high/low of the currently shown candles
+            # updates y_axis to the candle high/low of the currently shown candles
             self.y_axis.setRange(self.yaxis_lower_range, self.yaxis_upper_range)
-      
+
+            min_date = QDateTime.fromSecsSinceEpoch(
+                self.stock.dates[self.stock.index - self.candles_on_screen] //1000)
+            max_date = QDateTime.fromSecsSinceEpoch(self.stock.dates[self.stock.index - 1] // 1000)
+
+            # add leading/trailing space in "days" to the chart
+            min_date = min_date.addSecs(-(self.off_set * 3) * self.day_in_seconds)
+            max_date = max_date.addSecs(self.off_set * self.day_in_seconds)
+
+            self.x_axis.setRange(min_date, max_date)
+
 
     def create_view(self):
         """displays the candlestick charts, and sets the legend, axes etc.
@@ -148,27 +169,11 @@ class Chart:
         self.chart.setTitle(f"{self.stock.name} data from {self.stock.from_date}")
         self.chart.setAnimationOptions(QChart.AnimationOption.SeriesAnimations)
 
-        # create X axis and set range
-        x_axis = QDateTimeAxis()
-        x_axis.setFormat("MMM-yy")
-        x_axis.setTitleText("Date")
 
-        min_date = QDateTime.fromSecsSinceEpoch(
-            self.stock.dates[0] // 1000)  # this function expects milliseconds
-        max_date = QDateTime.fromSecsSinceEpoch(self.stock.dates[-1] // 1000)
-
-        # adds leading/trailing space in "days" so candlesticks don't go up to the borders (visually more pleasing)
-        min_date = min_date.addSecs(-7200 * 24)
-        max_date = max_date.addSecs(3600 * 24)
-
-        x_axis.setRange(min_date, max_date)
-
-        #setting the value spacing of the x-axis to 7 - TickCount(7)
-        x_axis.setTickCount(7)
-        self.chart.addAxis(x_axis, Qt.AlignmentFlag.AlignBottom)
-        self.stock.stock_series.attachAxis(x_axis)
-        self.stock.buy_signal_series.attachAxis(x_axis)
-        self.stock.sell_signal_series.attachAxis(x_axis)
+        self.chart.addAxis(self.x_axis, Qt.AlignmentFlag.AlignBottom)
+        self.stock.stock_series.attachAxis(self.x_axis)
+        self.stock.buy_signal_series.attachAxis(self.x_axis)
+        self.stock.sell_signal_series.attachAxis(self.x_axis)
 
         self.y_axis = QValueAxis()
         self.y_axis.setTitleText("Stock Price")
@@ -185,7 +190,7 @@ class Chart:
         # creating the list of steps to pass to run_animation() using the loop below
         animation_list = []
         for i in range(len(self.stock.dates) - self.stock.index):
-            #set time interval to 1.5 sec instead of using J as J was increasing time interval exponentionally
+            # set time interval to 1.5 sec instead of using J as J was increasing time interval exponentionally
             animation_list.append((self.add_new_candle, 0.5))
 
         self.animation = Animation(animation_list)
@@ -197,7 +202,7 @@ class Chart:
 
         return chart_view
 
-# updates the OHLC data in a new widget if the candle is moused over
+    # updates the OHLC data in a new widget if the candle is moused over
     def update_label(self, hovered, candlestick_set, index):
         if hovered:
             date = QDateTime.fromMSecsSinceEpoch(self.stock.dates[index]).toString("yyyy-MM-dd")
@@ -210,4 +215,6 @@ class Chart:
                 f"Date: {date} | Open: {open_price:.2f} | Close: {close_price:.2f} | High: {high:.2f} | Low: {low:.2f}")
         else:
             self.info_label.setText("Hover over a candlestick to see details")
+
+
 
