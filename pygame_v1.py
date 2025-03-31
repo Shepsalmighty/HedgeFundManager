fb0783671f70dfcc3bb91ea9f959003c9aafed11
sheps <sheps@twitch.tv>
2025-03-31 20:02:52 +0100

    add nothing of value just playing with pygame

---
+++ pygame_v1.py
@@ -0,0 +1,63 @@
+import pygame
+# pygame setup
+
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+dt = 0
+player_speed = 0
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+while running:
+    player_click = pygame.mouse.get_pos()
+
+    # player_acc = max()
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("black")
+    # pygame.draw.circle(screen, "red", player_pos, 40)
+    rect = pygame.Rect(player_pos[0],player_pos[1],42,69)
+    # pygame.draw.rect(screen, color="green", rect=rect, width=10)
+    pygame.draw.rect(screen, (200, 200, 200), rect)
+
+
+    x_acc = abs(player_pos[0] - player_click[0])
+    y_acc = abs(player_pos[1] - player_click[1])
+
+    if player_pos[0] < player_click[0]:
+        player_pos.x += (player_speed + x_acc) * dt
+    if player_pos[0] > player_click[0]:
+        player_pos.x -= (player_speed + x_acc) * dt
+    if player_pos[1] < player_click[1]:
+        player_pos.y += (player_speed + y_acc) * dt
+    if player_pos[1] > player_click[1]:
+        player_pos.y -= (player_speed + y_acc) * dt
+
+
+
+    # keys = pygame.key.get_pressed()
+    # if keys[pygame.K_w]:
+    #     player_pos.y -= player_speed * dt
+    # if keys[pygame.K_s]:
+    #     player_pos.y += player_speed * dt
+    # if keys[pygame.K_a]:
+    #     player_pos.x -= player_speed * dt
+    # if keys[pygame.K_d]:
+    #     player_pos.x += player_speed * dt
+
+
+    # flip() the display to put your work on screen
+    pygame.display.flip() #INFO .flip() like flipping a page, i'd call it frame.update or something memba dis
+
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    dt = clock.tick(60) / 1000
+
+pygame.quit()
